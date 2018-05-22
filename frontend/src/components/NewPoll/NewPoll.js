3f8e23e1693d99e82b0ae939dd4592bb5c8616c7
Matthew Robertson <mattrobertson1414@gmail.com>
2018-05-22 00:26:03 -0600

    I created the form for the new poll

--- frontend/src/components/NewPoll/NewPoll.js
+++ frontend/src/components/NewPoll/NewPoll.js
@@ -3,10 +3,17 @@ import './NewPoll.css';
 //import propTypes from 'prop-types';
 
 const NewPoll = () => {
+  let optionOne, optionTwo;
 
   return (
     <div className="NewPoll">
-      NewPoll
+      <p className="NewPollTitle">Would You Rather</p>
+      <label htmlFor='optionOne'>Option 1</label>
+      <input ref={i=>optionOne=i} className="optionOne" placeholder='e.x. Do this'/>
+      <p className="or">or</p>
+      <label htmlFor='optionTwo'>Option 2</label>
+      <input ref={i=>optionTwo=i} className="optionTwo" placeholder='e.x Do that'/>
+      <button>Submit</button>
     </div>
   )
 }
